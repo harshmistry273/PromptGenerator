37b6e70f7626fe7f4a2ef9c24de338eb13f53a6c
niraj <nirajparadva2004@gmail.com>
2025-04-18 12:15:15 +0530

    Create Responsive Frontend

---
+++ front end/my-vite-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WebSocketClient from './components/WebSocketClient'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <WebSocketClient />
+    </>
+  )
+}
+
+export default App
